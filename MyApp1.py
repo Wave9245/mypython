80296fbc9a18fef313371cab34610753b7cc0452
Wave9245 <Microwave9245@gmail.com>
2024-03-28 11:13:02 +0700

    Create MyApp1.py

---
+++ MyApp1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('website Devevolping using Python')
+st.header('website Devevolping using Python')
+st.subheader('website Devevolping using Python')
+st.img('sea.jpg')
