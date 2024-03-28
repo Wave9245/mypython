f81ec4556cd223b29882e16cb43753588f53b454
Wave9245 <Microwave9245@gmail.com>
2024-03-28 11:29:04 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('🌙website Devevolping using Python🌙')
+st.title('website Devevolping using Python🌙')
 st.header('🌙website Devevolping using Python🌙')
 st.subheader('Woramet Kaichapoa')
 st.image('sea.jpg')
